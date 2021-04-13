f9a0ada9dd648cff0d217a9ab302f636e1444ada
DarrylGollan <darryl.gollan@gmail.com>
2021-04-13 14:48:27 -0400

    Completed writing and testing of first successful Green test Get_WhenCalled_ReturnsAllCustomers that tests the /api/customer get method for returning all customers. Also optimized code bringing in Fluent Assertions that allow fow writing cleaner tests.

--- GroceryStoreAPI.Tests/Customer/Customer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GroceryStoreAPI.Customer
-{
-    public class Customer
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
---
+++ GroceryStoreAPI.Tests/CustomerTests/CustomerTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using GroceryStoreAPI.Entities;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GroceryStoreAPI.CustomerTests
+{
+    public class CustomerTests : IClassFixture<WebApplicationFactory<GroceryStoreAPI.Startup>>
+    {
+        private readonly HttpClient _client;
+        private readonly string _baseURL;
+
+        public CustomerTests(WebApplicationFactory<GroceryStoreAPI.Startup> fixture)
+        {
+            _client = fixture.CreateClient();
+            _baseURL = "https://localhost:5001/api/customer";
+        }
+
+        [Fact]
+        public async Task Get_WhenCalled_ReturnsAllCustomers()
+        {
+            var response = await _client.GetAsync(_baseURL);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var customers = JsonConvert.DeserializeObject<List<Customer>>(await response.Content.ReadAsStringAsync());
+            customers.Should().HaveCount(3);
+        }
+    }
+}
