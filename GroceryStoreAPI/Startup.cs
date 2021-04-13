023bb379ff444226f5aec38f80aae14ba633a234
DarrylGollan <darryl.gollan@gmail.com>
2021-04-13 15:56:27 -0400

    Added Swagger.

--- GroceryStoreAPI/Startup.cs
+++ GroceryStoreAPI/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace GroceryStoreAPI
 {
@@ -22,6 +23,10 @@ namespace GroceryStoreAPI
         {
             services.AddApplicationServices(_config);
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Grocery Store API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,7 +35,15 @@ namespace GroceryStoreAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Grocery Store API v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
+
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
